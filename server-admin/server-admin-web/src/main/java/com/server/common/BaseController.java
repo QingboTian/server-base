d46865ed29afa93271fcb96335d3d9b846874292
tianqb <tqb820965236@163.com>
2020-10-21 18:33:08 +0800

    dev

--- server-admin/server-admin-web/src/main/java/com/server/common/BaseController.java
+++ server-admin/server-admin-web/src/main/java/com/server/common/BaseController.java
@@ -1,16 +1,25 @@
 package com.server.common;
 
+import com.alibaba.fastjson.JSONObject;
+import lombok.extern.slf4j.Slf4j;
+
 /**
 * @author tianqingbo3
 * @version 1.0
 * @date 2020/10/21 9:22
 */
+@Slf4j
 public class BaseController {
 
-    public ClientSource getClientSource() {
-        ClientSource clientSource = new ClientSource();
+    private static ClientSource clientSource = new ClientSource();
+
+    static {
         clientSource.setAppId("53783804-523d-49a0-99da-f044a718f8bd");
         clientSource.setAppName("server-admin");
+        log.info("Application clientSource init: {}", JSONObject.toJSONString(clientSource));
+    }
+
+    public static ClientSource getClientSource() {
         return clientSource;
     }
 
